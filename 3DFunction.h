07f5dab81fef951cb238853b9aecd3da429b43a8
KyouGenSo <k023g0083@g.neec.ac.jp>
2024-05-24 13:07:36 +0900

    DrawGrid関数できた

---
+++ 3DFunction.h
@@ -0,0 +1,42 @@
+﻿#pragma once
+#include<Vector3.h>
+#include<Vector3Function.h>
+#include<Matrix4x4.h>
+#include<Matrix4x4Function.h>
+#include<math.h>
+
+void DrawGrid(const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix) {
+	const float kGridHalfWidth = 2.0f; // グリッドの半分の幅
+	const uint32_t kSubdivision = 10; // 1分割数
+	const float kGridEvery = (kGridHalfWidth * 2.0f) / float(kSubdivision); // 1分割の長さ
+
+	// 奥から手前への線を順々に引いていく
+	for (uint32_t xIndex = 0; xIndex <= kSubdivision; xIndex++) {
+		// 上の情報を使ってワールド座標系上の始点と終点を求める
+		Vector3 worldStart(-kGridHalfWidth + kGridEvery * float(xIndex), -1.0f, kGridHalfWidth);
+		Vector3 worldEnd(-kGridHalfWidth + kGridEvery * float(xIndex), -1.0f, -kGridHalfWidth);
+		// スクリーン座標系まで変換をかける
+		Vector3 screenStart = TransForm(viewProjectionMatrix, worldStart);
+		Vector3 screenEnd = TransForm(viewProjectionMatrix, worldEnd);
+		// スクリーン座標系からビューポート座標系に変換をかける
+		screenStart = TransForm(viewportMatrix, screenStart);
+		screenEnd = TransForm(viewportMatrix, screenEnd);
+		// ライン描画
+		Novice::DrawLine(int(screenStart.x), int(screenStart.y), int(screenEnd.x), int(screenEnd.y), 0xAAAAAAFF);
+	}
+
+	// 左から右への線を順々に引いていく
+	for (uint32_t zIndex = 0; zIndex <= kSubdivision; zIndex++) {
+		// 上の情報を使ってワールド座標系上の始点と終点を求める
+		Vector3 worldStart(-kGridHalfWidth, -1.0f, kGridHalfWidth - kGridEvery * float(zIndex));
+		Vector3 worldEnd(kGridHalfWidth, -1.0f, kGridHalfWidth - kGridEvery * float(zIndex));
+		// スクリーン座標系まで変換をかける
+		Vector3 screenStart = TransForm(viewProjectionMatrix, worldStart);
+		Vector3 screenEnd = TransForm(viewProjectionMatrix, worldEnd);
+		// スクリーン座標系からビューポート座標系に変換をかける
+		screenStart = TransForm(viewportMatrix, screenStart);
+		screenEnd = TransForm(viewportMatrix, screenEnd);
+		// ライン描画
+		Novice::DrawLine(int(screenStart.x), int(screenStart.y), int(screenEnd.x), int(screenEnd.y), 0xAAAAAAFF);
+	}
+}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <Novice.h>
 #include <Matrix4x4Function.h>
 #include <Vector3Function.h>
+#include <3DFunction.h>
+#include <imgui.h>
+#include <ImGuiManager.h>
+#include <imgui_impl_dx12.h>
 
 const char kWindowTitle[] = "LE2A_10_キョウ_ゲンソ";
 
@@ -14,25 +18,26 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 
 	// キー入力結果を受け取る箱
-	char keys[256] = {0};
-	char preKeys[256] = {0};
+	char keys[256] = { 0 };
+	char preKeys[256] = { 0 };
 
-	Vector3 v1(1.2f, -3.9f, 2.5f);
-	Vector3 v2(2.8f, 0.4f, -1.3f);
+	Vector3 v1(3.0f, 0.0f, -2.0f);
+	Vector3 v2(5.0f, 3.0f, 2.0f);
 	Vector3 cross = Cross(v1, v2);
 
 	Vector3 localVertices[3] = { Vector3(0.0f, 1.0f, 0.0f), Vector3(1.0f, -1.0f, 0.0f), Vector3(-1.0f, -1.0f, 0.0f) };
 	Vector3 scale(1.0f, 1.0f, 1.0f);
 	Vector3 rotate(0.0f, 0.0f, 0.0f);
 	Vector3 translate(0.0f, 0.0f, 0.0f);
+
 	Vector3 cameraPosition(0.0f, 0.0f, 10.0f);
 
 	Matrix4x4 worldMatrix = MakeAffineMatrix(scale, rotate, translate);
 	Matrix4x4 cameraMatrix = MakeAffineMatrix(Vector3(1.0f, 1.0f, 1.0f), Vector3(0.0f, 0.0f, 0.0f), cameraPosition);
-	Matrix4x4 viewMatrix = Inverse(cameraMatrix); 
+	Matrix4x4 viewMatrix = Inverse(cameraMatrix);
 	Matrix4x4 projectionMatrix = MakePerspectiveMatrix(0.45f, (float)windowX / (float)windowY, 0.1f, 100.0f);
 	Matrix4x4 wvpMatrix = Multiply(worldMatrix, Multiply(viewMatrix, projectionMatrix));
-	Matrix4x4 viewportMtrix = MakeViewportMatrix(0, 0, float(windowX), float(windowY),0.0f, 1.0f);
+	Matrix4x4 viewportMtrix = MakeViewportMatrix(0, 0, float(windowX), float(windowY), 0.0f, 1.0f);
 	Vector3 screenVertices[3];
 
 
@@ -61,7 +66,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			translate.x += 0.1f;
 		}
 
-		rotate.y += 0.05f;
+		//rotate.y += 0.05f;
 
 		if (rotate.y > 6.28f) {
 			rotate.y = 0.0f;
@@ -88,11 +93,20 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 		Vector3ScreenPrint(0, 0, cross, "Cross");
 
+		DrawGrid(wvpMatrix, viewportMtrix);
+
 		Novice::DrawTriangle(
-			int(screenVertices[0].x), int(screenVertices[0].y), 
-			int(screenVertices[1].x), int(screenVertices[1].y), 
+			int(screenVertices[0].x), int(screenVertices[0].y),
+			int(screenVertices[1].x), int(screenVertices[1].y),
 			int(screenVertices[2].x), int(screenVertices[2].y),
 			RED, kFillModeSolid);
+
+		ImGui::Begin("Transform");
+		ImGui::SliderFloat3("Scale", &scale.x, 0.0f, 10.0f);
+		ImGui::SliderFloat3("Rotate", &rotate.x, 0.0f, 6.28f);
+		ImGui::SliderFloat3("Translate", &translate.x, -10.0f, 10.0f);
+		ImGui::End();
+
 
 		///-------------------///
 		/// ↑描画処理ここまで///
